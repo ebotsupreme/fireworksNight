df2cfcec9dc24aca60cbc7316858976957cb9f5a
Eddie Jung <ejung716@gmail.com>
2021-08-26 10:05:39 -0700

    initial commit.

---
+++ Project20/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Eddie Jung on 8/26/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
